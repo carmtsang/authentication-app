ca3ab36cfb135d5f8f8cfe653dafbb7e2b8b491a
carmtsang <carmen.tsang1208@hotmail.com>
2022-11-08 21:49:23 -0800

    install fontawesome

--- pages/login.tsx
+++ pages/login.tsx
@@ -3,6 +3,9 @@ import Head from "next/head";
 import Image from "next/image";
 import Input from "../components/Input";
 import InputLabel from "../components/InputLabel";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
 
 export default function Login() {
   return (
@@ -11,26 +14,20 @@ export default function Login() {
         <title>Log In</title>
       </Head>
 
-      <main className="flex justify-center py-12 mt-36">
-        <div>
+      <main className="flex justify-center py-8 mt-36 ">
+        <div className="w-1/3">
           <div className="bg-white py-10 shadow-xl ring-1 ring-gray-900/5  sm:rounded-lg sm:px-10">
-            <div className="w-full max-w-md space-y-8 ">
-              {/* <Image
-                className="mx-auto h-12 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt="Your Company"
-                width={300}
-                height={300}
-              /> */}
+            <div className="max-w-md space-y-8 ">
               <p className="mt-2 text-sm text-gray-600">Company Name</p>
+
               <h2 className="mt-3 text-2xl tracking-tight text-gray-900">
                 Login
               </h2>
             </div>
 
-            <form className="mt-4 space-y-6" action="#" method="POST">
+            <form className="mt-3 space-y-6" action="#" method="POST">
               <input type="hidden" name="remember" defaultValue="true" />
-              <div className="-space-y-px rounded-md shadow-sm">
+              <div className="space-y-px rounded-md shadow-sm">
                 <div>
                   <InputLabel type="email">Email</InputLabel>
                   <Input type="email" placeholder="Email Address" />
@@ -44,28 +41,21 @@ export default function Login() {
               <div>
                 <button
                   type="submit"
-                  className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                  className="group relative flex w-full justify-center rounded-md border border-transparent bg-blue-600 py-2 px-4 text-sm font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
                 >
-                  <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                    <LockClosedIcon
-                      className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
-                      aria-hidden="true"
-                    />
-                  </span>
                   Sign in
                 </button>
               </div>
             </form>
 
-            <div className="flex-row items-center justify-between font-small text-gray-600">
-              <div className="flex items-center">
-                <p>or continue with these social profile</p>
-              </div>
+            <div className="flex-row text-center items-center mt-5 text-sm text-gray-500">
+              <p>or continue with these social profile</p>
               <div>
+                <FontAwesomeIcon icon={faFacebook} />
                 <p>social icons go here</p>
               </div>
 
-              <div className="flex items-center mt-7 ">
+              <div className="flex justify-center mt-7 ">
                 <p className="pr-2">Don&#39;t have an account yet? </p>
                 <a href="#" className="text-indigo-600 hover:text-indigo-500">
                   Register
