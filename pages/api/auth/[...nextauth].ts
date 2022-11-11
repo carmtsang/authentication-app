0ccd5d9525fb1f37ec6257c6edfab8da2e17452a
carmtsang <carmen.tsang1208@hotmail.com>
2022-11-10 17:58:58 -0800

    create file & add github

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth/next";
+import GithubProvider from "next-auth/providers/github";
+
+export default NextAuth({
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+      scope: "read:user",
+    }),
+  ],
+  database: process.env.DATABASE_URL,
+});
