401443df04057469fea75cc43d37ddb7c268d520
carmtsang <carmen.tsang1208@hotmail.com>
2022-11-10 19:40:37 -0800

    configure shared shession state

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,15 @@ import type { AppProps } from "next/app";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false;
+import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
