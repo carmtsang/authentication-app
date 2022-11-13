59ebca415b72b5217c25907abfa25c51b6bd368f
carmtsang <carmen.tsang1208@hotmail.com>
2022-11-12 19:03:48 -0800

    change a tag to button

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,31 +2,18 @@ import { useSession, signIn, signOut } from "next-auth/react";
 import Link from "next/link";
 
 export const Header = () => {
-  const handleSignIn = (e: any) => {
-    e.preventDefault();
-    signIn();
-  };
-
-  const handleSignOut = (e: any) => {
-    e.preventDefault();
-    signOut;
-  };
-
   const { data: session } = useSession();
 
   return (
     <div className="header">
       <Link href="/">LOGO</Link>
       {session && (
-        <a href="#" onClick={handleSignOut}>
-          Sign Out
-        </a>
+        <>
+          Signed in as {session.user.email} <br />{" "}
+          <button onClick={() => signOut()}>Sign Out</button>
+        </>
       )}
-      {!session && (
-        <a href="#" onClick={handleSignIn}>
-          Sign In
-        </a>
-      )}
+      {!session && <button onClick={() => signIn()}>Sign In</button>}
     </div>
   );
 };
