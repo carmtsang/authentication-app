4256bd24fd229cf03b8bb5b0ccb71f8e3bbb8060
carmtsang <carmtsang89@hotmail.com>
2023-05-10 21:58:59 -0700

    center card & use login template

--- app/page.js
+++ app/page.js
@@ -1,5 +1,11 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="h-screen flex items-center justify-center">
+      <div className="w-screen max-w-md flex-auto overflow-hidden rounded-3xl bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5">
+        <p>user page goes here</p>
+      </div>
+    </main>
+  );
 }
