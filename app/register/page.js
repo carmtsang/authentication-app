cf6fb79400405f61d5e7fd2006d1e867497ec2bc
carmtsang <carmtsang89@hotmail.com>
2023-05-11 21:28:56 -0700

    update text

--- app/register/page.js
+++ app/register/page.js
@@ -1,19 +1,31 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const MEMBER_MSG = "Already a member?";
+const LOGIN = "Login";
+const REGISTER_TITLE = "Join thousands of learners from around the world";
+const REGISTER_MSG =
+  "Master web development by making real life projects. There are mutiple paths for you to choose";
+const REGISTER_BTN = "Start coding now";
+
 export default function Register() {
   return (
     <main className="h-screen flex items-center justify-center">
       <div className="w-screen max-w-md flex-auto overflow-hidden rounded-3xl bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5">
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img
-              className="mx-auto h-10 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-              alt="Your Company"
+            <Image
+              src="/devchallenges.svg"
+              width={50}
+              height={10}
+              alt="devchallenge logo"
+              className="h-6 w-auto"
             />
-            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-              Sign in to your account
+            <h2 className="mt-6 text-2xl font-bold leading-9 tracking-tight text-gray-900">
+              {REGISTER_TITLE}
             </h2>
           </div>
-
+          <div className="mt-1">{REGISTER_MSG}</div>
           <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
             <form className="space-y-6" action="#" method="POST">
               <div>
@@ -28,6 +40,7 @@ export default function Register() {
                     id="email"
                     name="email"
                     type="email"
+                    placeholder="Email"
                     autoComplete="email"
                     required
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -43,20 +56,13 @@ export default function Register() {
                   >
                     Password
                   </label>
-                  <div className="text-sm">
-                    <a
-                      href="#"
-                      className="font-semibold text-indigo-600 hover:text-indigo-500"
-                    >
-                      Forgot password?
-                    </a>
-                  </div>
                 </div>
                 <div className="mt-2">
                   <input
                     id="password"
                     name="password"
                     type="password"
+                    placeholder="Password"
                     autoComplete="current-password"
                     required
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -69,19 +75,19 @@ export default function Register() {
                   type="submit"
                   className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                  Sign in
+                  {REGISTER_BTN}
                 </button>
               </div>
             </form>
 
             <p className="mt-10 text-center text-sm text-gray-500">
-              Not a member?{" "}
-              <a
-                href="#"
+              {MEMBER_MSG}{" "}
+              <Link
+                href="/login"
                 className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
               >
-                Start a 14 day free trial
-              </a>
+                {LOGIN}
+              </Link>
             </p>
           </div>
         </div>
