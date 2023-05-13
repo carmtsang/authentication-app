58978f5e6c3e099122d1f6d85aff7cd8fbfb3c0d
carmtsang <carmtsang89@hotmail.com>
2023-05-12 22:32:55 -0700

    remove label

--- app/login/page.js
+++ app/login/page.js
@@ -1,3 +1,4 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -8,7 +9,7 @@ const LOGIN = "Login";
 export default function Login() {
   return (
     <main className="h-screen flex items-center justify-center">
-      <div className="w-screen max-w-md flex-auto overflow-hidden rounded-3xl bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5">
+      <div className="w-screen max-w-sm flex-auto overflow-hidden rounded-3xl bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5">
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
             <Image
@@ -18,42 +19,23 @@ export default function Login() {
               alt="devchallenge logo"
               className="h-6 w-auto"
             />
-
             <h2 className="mt-6 text-2xl font-bold leading-9 tracking-tight text-gray-900">
               {LOGIN}
             </h2>
           </div>
-
           <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
             <form className="space-y-6" action="#" method="POST">
               <div>
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Email address
-                </label>
                 <div className="mt-2">
                   <input
                     id="email"
                     name="email"
                     type="email"
                     autoComplete="email"
-                    placeholder="Email"
+                    placeholder=" Email"
                     required
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
-                </div>
-              </div>
-
-              <div>
-                <div className="flex items-center justify-between">
-                  <label
-                    htmlFor="password"
-                    className="block text-sm font-medium leading-6 text-gray-900"
-                  >
-                    Password
-                  </label>
                 </div>
                 <div className="mt-2">
                   <input
@@ -61,7 +43,7 @@ export default function Login() {
                     name="password"
                     type="password"
                     autoComplete="current-password"
-                    placeholder="Password"
+                    placeholder=" Password"
                     required
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
--- app/register/page.js
+++ app/register/page.js
@@ -11,7 +11,7 @@ const REGISTER_BTN = "Start coding now";
 export default function Register() {
   return (
     <main className="h-screen flex items-center justify-center">
-      <div className="w-screen max-w-md flex-auto overflow-hidden rounded-3xl bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5">
+      <div className="w-screen max-w-sm flex-auto overflow-hidden rounded-3xl bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5">
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
             <Image
@@ -29,40 +29,24 @@ export default function Register() {
           <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
             <form className="space-y-6" action="#" method="POST">
               <div>
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Email address
-                </label>
                 <div className="mt-2">
                   <input
                     id="email"
                     name="email"
                     type="email"
-                    placeholder="Email"
+                    placeholder=" Email"
                     autoComplete="email"
                     required
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
-              </div>
 
-              <div>
-                <div className="flex items-center justify-between">
-                  <label
-                    htmlFor="password"
-                    className="block text-sm font-medium leading-6 text-gray-900"
-                  >
-                    Password
-                  </label>
-                </div>
                 <div className="mt-2">
                   <input
                     id="password"
                     name="password"
                     type="password"
-                    placeholder="Password"
+                    placeholder=" Password"
                     autoComplete="current-password"
                     required
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
