e1d2ec6d2e43fda88e8586fea1a390098f5c44e6
carmtsang <carmtsang89@hotmail.com>
2023-05-20 14:43:35 -0700

    put logo on nav

--- components/navbar.js
+++ components/navbar.js
@@ -1,9 +1,21 @@
 "use client";
 
+import Image from "next/image";
+
 export default function Navbar() {
   return (
-    <nav>
-      <p>Nav bar goes here</p>
+    <nav className="flex justify-between p-3">
+      <Image
+        src="/devchallenges.svg"
+        width={50}
+        height={10}
+        alt="devchallenge logo"
+        className="h-5 w-auto"
+      />
+      <div className="flex">
+        <Image src="/" width={50} height={10} alt="user" />
+        <span>USER NAME</span>
+      </div>
     </nav>
   );
 }
