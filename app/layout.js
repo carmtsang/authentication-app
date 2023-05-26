9941d5728a87a3f5973392e92981d723464d87ed
carmtsang <carmtsang89@hotmail.com>
2023-05-25 21:20:01 -0700

    add login service, prisma adapter & sesh provider

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import Footer from "@/app/components/footer";
 import Navbar from "@/app/components/navbar";
+import Provider from "./components/Provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,13 +15,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        <main className="h-screen flex flex-col items-center justify-center">
-          <div className="w-screen max-w-sm md:max-w-md overflow-hidden flex-none rounded-3xl bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5">
-            {children}
-          </div>
-          <Footer />
-        </main>
+        <Provider>
+          <Navbar />
+          <main className="h-screen flex flex-col items-center justify-center">
+            <div className="w-screen max-w-sm md:max-w-md overflow-hidden flex-none rounded-3xl bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5">
+              {children}
+            </div>
+            <Footer />
+          </main>
+        </Provider>
       </body>
     </html>
   );
