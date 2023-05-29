7ea8de0718a8e910090907ab2f7ce4178aabbfb9
carmtsang <carmtsang89@hotmail.com>
2023-05-28 19:03:34 -0700

    include links

--- app/components/Social.js
+++ app/components/Social.js
@@ -1,32 +1,35 @@
 "use client";
 
 import Image from "next/image";
+import Link from "next/link";
 
 const SOCIAL_PROFILE_TXT = "or continue with these social profile";
 const SOCIAL_ICONS = [
   {
     name: "Google",
     image: "/Google.svg",
-    link: "",
+    link: "/api/auth/google",
   },
   {
     name: "Facebook",
     image: "/Facebook.svg",
-    link: "",
+    link: "/api/auth/facebook",
   },
   {
     name: "Twitter",
     image: "/Twitter.svg",
-    link: "",
+    link: "/api/auth/twitter",
   },
   {
     name: "Github",
     image: "/Github.svg",
-    link: "",
+    link: "/api/auth/github",
   },
 ];
 
 export default function SocialProfile() {
+  const host = process.env.NEXT_PUBLIC_URL;
+
   return (
     <>
       <p className="mt-10 text-center text-sm text-gray-500">
@@ -34,15 +37,9 @@ export default function SocialProfile() {
       </p>
       <div className="mt-5 flex flex-row justify-around">
         {SOCIAL_ICONS.map((icon) => (
-          <Image
-            key={icon.name}
-            src={icon.image}
-            alt={icon.name}
-            width={50}
-            height={50}
-            className="cursor-pointer"
-            onClick={() => console.log("click")}
-          />
+          <Link key={icon.name} href={`${host}${icon.link}`}>
+            <Image src={icon.image} alt={icon.name} width={50} height={50} />
+          </Link>
         ))}
       </div>
     </>
