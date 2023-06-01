e0c1611fd06c3bac2ebfc195b36e9ee935e25562
carmtsang <carmtsang89@hotmail.com>
2023-05-31 21:12:58 -0700

    nav only visible with session

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -1,21 +1,28 @@
 "use client";
 
+import { useSession } from "next-auth/react";
 import Image from "next/image";
 
 export default function Navbar() {
+  const { data: session } = useSession();
+
   return (
-    <nav className="flex justify-between p-3">
-      <Image
-        src="/devchallenges.svg"
-        width={50}
-        height={10}
-        alt="devchallenge logo"
-        className="h-5 w-auto"
-      />
-      <div className="flex">
-        <Image src="/" width={50} height={10} alt="user" />
-        <span>USER NAME</span>
-      </div>
-    </nav>
+    <>
+      {session && (
+        <nav className="flex justify-between p-3">
+          <Image
+            src="/devchallenges.svg"
+            width={50}
+            height={10}
+            alt="devchallenge logo"
+            className="h-5 w-auto"
+          />
+          <div className="flex">
+            <Image src="/" width={50} height={10} alt="user" />
+            <span>{session?.user?.name}</span>
+          </div>
+        </nav>
+      )}
+    </>
   );
 }
